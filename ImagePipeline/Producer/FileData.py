a9eb8722eae48e495f378d2c240f19ea452b9298
MVPatM <76089856+MVPatM@users.noreply.github.com>
2025-12-10 00:06:57 +0900

    Implement FileData class for image handling

---
+++ ImagePipeline/Producer/FileData.py
@@ -0,0 +1,35 @@
+from PIL import Image, ImageFile
+from io import BytesIO
+import os, aiofiles
+
+class FileData():
+    def __init__(self, img_byte: bytes, file_name: str):
+        self.imgbyte = img_byte
+        self.file_name = file_name
+        self.file_format = Image.open(BytesIO(self.imgbyte)).format.lower()
+    
+    def get_imgfile(self) -> ImageFile:
+        return Image.open(BytesIO(self.imgbyte))
+    
+    def get_imgbyte(self) -> bytes:
+        return self.imgbyte
+    
+    def get_imgBytesIO(self) -> BytesIO:
+        return BytesIO(self.imgbyte)
+    
+    def get_filename(self) -> str:
+        return self.file_name
+    
+    def get_file_fullname(self) -> str:
+        return self.get_filename() + "." + self.get_fileformat()
+    
+    def get_fileformat(self) -> str:
+        return self.file_format
+    
+    async def save_imgfile(self, path: str) -> None:
+        async with aiofiles.open(path, mode='wb') as f:
+            await f.write(self.imgbyte)
+
+    async def store_tmp(self) -> None:
+        tmp_path = os.path.dirname(os.path.abspath(__file__)) + '/tmp/' + self.get_file_fullname()
+        await self.save_imgfile(tmp_path)
