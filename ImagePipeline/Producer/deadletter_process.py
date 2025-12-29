740a4855e5b6a45d1c2f0af6f51d6b618fc0b76c
MVPatM <76089856+MVPatM@users.noreply.github.com>
2025-12-30 00:32:57 +0900

    Refactor deadletter_process to use KafkaService

--- ImagePipeline/Producer/deadletter_process.py
+++ ImagePipeline/Producer/deadletter_process.py
@@ -1,52 +1,34 @@
-from confluent_kafka import Producer
-from ImagePipeline.Producer.producer_config import normal_producer_conf, db_url
-from confluent_kafka.serialization import StringDeserializer
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from ImagePipeline.Producer.producer_dao import DeadLetterDAO
-import os, portalocker
+from ImagePipeline.Producer.Service.KafkaService import KafkaService
+from ImagePipeline.Producer.utils.db_utils import get_db_session
+from ImagePipeline.Producer.CRUD.producer_dao import DeadLetterDAO
+import os
 from datetime import datetime
 
 sessionlocal = None
 string_deserializer = None
 
 def main():
-    producer = Producer(normal_producer_conf)
+    kafka_producer = KafkaService()
     
     global string_deserializer
-    string_deserializer = StringDeserializer('utf_8')
+    string_deserializer = kafka_producer.get_string_deserializer()
     
-    global sessionlocal
-    engine = create_engine(db_url, echo=True)
-    sessionlocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-    
-    with sessionlocal() as session:
+    with get_db_session() as session:
         dead_letter_dao = DeadLetterDAO(session)
         batches = dead_letter_dao.get_dead_letter()
         
         for i, batch in enumerate(batches):
             for dead_letter in batch:
-                producer.produce(topic=dead_letter.topic,
-                                key=dead_letter.key,
-                                value=dead_letter.value, 
-                                callback=delivery_report)
-                producer.flush()
-
-def write_log_sync(log_file_name: str, content: str) -> None:
-    try:
-        with portalocker.Lock(log_file_name, 'a', timeout=5) as file:
-            file.write(content)
-    except Exception as e:
-        print(f"error {e} occurs")
+                kafka_producer.produceDeadLetter(dead_letter.topic,
+                                                 dead_letter.key,
+                                                 dead_letter.value,
+                                                 delivery_report)
 
 # Callback function for sending the result of request
 def delivery_report(err, msg):
     if err is not None:
         console_msg = f"Delivery failed for record {msg.value()} at {datetime.now()}: {err}"
         print(console_msg)
-        
-        # write failure log
-        write_log_sync("failure_log.txt", console_msg + "\n")
     else:
         console_msg = f"Record {msg.value()} successfully produced {msg.topic()} [{msg.partition()}] at offset {msg.offset()} and latency is {msg.latency()}"
         print(console_msg)
