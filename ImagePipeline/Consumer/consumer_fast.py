b327d7b413e0a06806687d58bfc03801aed131b0
MVPatM <76089856+MVPatM@users.noreply.github.com>
2026-02-04 23:54:07 +0900

    Refactor consumer_fast.py to simplify image processing

--- ImagePipeline/Consumer/consumer_fast.py
+++ ImagePipeline/Consumer/consumer_fast.py
@@ -9,20 +9,11 @@ from PIL import Image
 from io import BytesIO
 import numpy as np
 import albumentations as A
-from transformers import pipeline, Pipeline
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from ImagePipeline.Consumer.consumer_dao import ImageDataDAO
 
 def get_image(s3_client, s3_url: str) -> BytesIO:
     buffer = BytesIO()
     s3_client.download_fileobj(bucket_name, s3_url, buffer)
     return buffer
-    
-def get_class(image: BytesIO, classifier: Pipeline) -> str:
-    image = Image.open(image) # pillow로 변환
-    result = classifier(image)
-    return result[0]['label']
 
 # pillow로 변환 필요함
 def image_augment(image: BytesIO) -> BytesIO: 
@@ -35,10 +26,9 @@ def image_augment(image: BytesIO) -> BytesIO:
     pil_img.save(buffer, format = 'png')
     return buffer
 
-def get_image_from_kafka(s3_client, sessionlocal) -> None:
+def get_image_from_kafka(s3_client) -> None:
     deserializer = ProtobufDeserializer(imagemetadata_pb2.ImageMetaData, {'use.deprecated.format': False})
     string_deserializer = StringDeserializer('utf_8')
-    classifier = pipeline("image-classification", model="google/vit-base-patch16-224")
     
     # subscribe to kafka topic
     consumer = Consumer(consumer_conf)
@@ -56,7 +46,6 @@ def get_image_from_kafka(s3_client, sessionlocal) -> None:
         # get image from s3
         s3_url = response_data.s3_url
         image_bytesIO = get_image(s3_client, s3_url)
-        image_class = get_class(image_bytesIO, classifier)
         image_bytesIO = image_augment(image_bytesIO)
         
         try:
@@ -64,11 +53,6 @@ def get_image_from_kafka(s3_client, sessionlocal) -> None:
             upload_s3_url = "upload_test/" + string_deserializer(msg.key())
             image_bytesIO.seek(0)
             s3_client.upload_fileobj(image_bytesIO, bucket_name, upload_s3_url)
-            
-            # upload metadata to mysql 
-            with sessionlocal() as session:
-                dao = ImageDataDAO(session)
-                dao.add_image_data(s3_url, image_class)
             
             # Commit the message directly
             consumer.commit(message=msg, asynchronous=False)
@@ -83,16 +67,12 @@ def MiddleWare() -> None:
                              aws_access_key_id = aws_access_key_id,
                              aws_secret_access_key = aws_secret_access_key)
     
-    # set to mysql config
-    engine = create_engine(db_url, echo=True)
-    sessionlocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-    
     # multiprocessing
     procs = []
     for i in range(3):
         p = multiprocessing.Process(
             target=get_image_from_kafka,
-            args=(s3_client, sessionlocal)
+            args=(s3_client,)
         )
         procs.append(p)
         p.start()
